ed1c77f06df3690cdb72a4cf56cd59f9afb45f6b
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-05 19:32:02 -0700

    working debugger for firebase

--- printerapi/src/App.js
+++ printerapi/src/App.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import style from "./style/index.scss";
 import firebase from "./database/firebase.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Home</p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paper: [],
+    };
+  }
+
+  componentDidMount() {
+    console.log(firebase.firestore().collection("paper"));
+    console.log(firebase.database().ref("paper"));
+    firebase
+      .firestore()
+      .collection("paper")
+      .onSnapshot((snapshot) => {
+        debugger;
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Home</p>
+          <p>Paper</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
+//export default App;
