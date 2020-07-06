6c32d0f9c0a8f138144559ac1e8c6959ea9c8d17
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-05 17:11:09 -0700

    clearing of the homepage

--- printerapi/src/App.js
+++ printerapi/src/App.js
@@ -1,21 +1,12 @@
 import React from "react";
 import style from "./style/index.scss";
+import firebase from "./database/firebase.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Home</p>
       </header>
     </div>
   );
