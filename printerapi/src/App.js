9d7fe0d8072de8b56e9a4c57b3a427053b233496
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-06 00:45:19 -0700

    remove console logs for firebase data pull

--- printerapi/src/App.js
+++ printerapi/src/App.js
@@ -11,8 +11,6 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    console.log(firebase.firestore().collection("paper"));
-    console.log(firebase.database().ref("paper"));
     firebase
       .firestore()
       .collection("paper")
