3a6dbbe56b073b0ec036f8d8238dbd75eeae0333
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-07 20:02:52 -0700

    removing testing

--- printerapi/src/App.js
+++ printerapi/src/App.js
@@ -11,39 +11,19 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({ paper: 2 });
     firebase
       .firestore()
       .collection("paper")
       .onSnapshot((chicken) => {
-        const data = chicken.docs.map((doc) => doc.data());
-        console.log(data);
-        const descriptions = [];
-        const sizes = [];
-        const names = [];
-        chicken.forEach(function (doc) {
-          names.push(doc.id);
-          descriptions.push(doc.data().description);
-          sizes.push(doc.data().size);
-          console.log(doc.id, "has the content: ", doc.data());
-        });
-        console.log("Names: ", names.join(", "));
-        console.log("Descriptions: ", descriptions.join(", "));
-        console.log("Sizes: ", sizes.join(", "));
-        this.setState({ paper: 3 });
       });
   }
 
   render() {
-    //const fast = this.state;
-    console.log("this is the yeah: " + this.state.yeah);
     return (
       <div className="App">
         <header className="App-header">
           <p>Home</p>
           <p>Paper</p>
-          <p>{this.state.paper}</p>
-          <p>{this.state.yeah.id}</p>
             <div key={paper.id}>
             </div>
         </header>
