9f41d280752bef907a2a9849c3ff77b0ef6f590f
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-08 00:48:22 -0700

    Create the estimate page

---
+++ printerapi/src/pages/estimate/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import style from "./index.module.scss";
+
+class Estimate extends React.Component {
+  render() {
+    return (
+      <div className={style.mainContainer}>
+        <section className={style.heroContainer}>
+          <h1>Estimate</h1>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Estimate;
