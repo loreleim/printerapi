0ffd81b3160e62d18d80737c4a2c15c1515ed99e
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-07 19:24:58 -0700

    pushing all the iterations I tried before success

--- printerapi/src/App.js
+++ printerapi/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./style/index.scss";
 import firebase from "./database/firebase.js";
 
@@ -7,32 +7,44 @@ class App extends React.Component {
     super(props);
     this.state = {
       paper: [],
+      yeah: [],
     };
   }
 
   componentDidMount() {
+    this.setState({ paper: 2 });
     firebase
       .firestore()
       .collection("paper")
-      .onSnapshot(function (chicken) {
+      .onSnapshot((chicken) => {
+        const data = chicken.docs.map((doc) => doc.data());
+        console.log(data);
         const descriptions = [];
         const sizes = [];
+        const names = [];
         chicken.forEach(function (doc) {
+          names.push(doc.id);
           descriptions.push(doc.data().description);
           sizes.push(doc.data().size);
           console.log(doc.id, "has the content: ", doc.data());
         });
+        console.log("Names: ", names.join(", "));
         console.log("Descriptions: ", descriptions.join(", "));
         console.log("Sizes: ", sizes.join(", "));
+        this.setState({ paper: 3 });
       });
   }
 
   render() {
+    //const fast = this.state;
+    console.log("this is the yeah: " + this.state.yeah);
     return (
       <div className="App">
         <header className="App-header">
           <p>Home</p>
           <p>Paper</p>
+          <p>{this.state.paper}</p>
+          <p>{this.state.yeah.id}</p>
         </header>
       </div>
     );
