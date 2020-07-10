0cf123732e277642bb3c9f1a28416b007f39bf0a
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-09 22:42:38 -0700

    temp login page

---
+++ printerapi/src/pages/login/index.js
@@ -0,0 +1,45 @@
+import React, { useCallback, useContext } from "react";
+import { withRouter, Redirect } from "react-router";
+import firebase from "../../database/firebase";
+import { AuthContext } from "../../database/auth";
+
+const Login = ({ history }) => {
+  const handleLogin = useCallback(
+    async (event) => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        firebase.auth().signInWithEmailAndPassword(email.value, password.value);
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
+
+  const { currentUser } = useContext(AuthContext);
+
+  if (currentUser) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <div>
+      <h1>Log in</h1>
+      <form onSubmit={handleLogin}>
+        <label>
+          Email
+          <input name="email" type="email" placeholder="Email" />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" placeholder="Password" />
+        </label>
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
+};
+
+export default withRouter(Login);
