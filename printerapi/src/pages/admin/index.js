0956d9feeb3f8688a530f40742e8aa22e367f503
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-10 00:03:10 -0700

    temp Signup page
    need to change this to be something a higher level admin can do

---
+++ printerapi/src/pages/admin/index.js
@@ -0,0 +1,45 @@
+import React, { useCallback } from "react";
+import { withRouter } from "react-router";
+import firebase from "../../database/firebase";
+
+const SignUp = ({ history }) => {
+  const handleSignUp = useCallback(
+    async (event) => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        await firebase
+          .auth()
+          .createUserWithEmailAndPassword(email.value, password.value);
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
+
+  return (
+    <div>
+      <h1>Hi Admin</h1>
+      <h2>Create new User</h2>
+      <form onSubmit={handleSignUp}>
+        <label>
+          Name
+          <input name="email" type="text" placeholder="Firstname" />
+        </label>
+        <label>
+          Email
+          <input name="email" type="email" placeholder="Email" />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" placeholder="Password" />
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default withRouter(SignUp);
