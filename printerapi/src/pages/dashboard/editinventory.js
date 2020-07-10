4d4deaa61fcd08f79fdd6eca6b55ca8dfdf742f5
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-10 00:09:18 -0700

    define states to allow change call in function

---
+++ printerapi/src/pages/dashboard/editinventory.js
@@ -0,0 +1,10 @@
+    this.state = {
+      paper: [],
+      newpaperid: "",
+      PaperName: "",
+      NewDescription: "",
+      NewReams: 0,
+      NewSheetsPerReam: 0,
+      NewUses: [],
+      NewWeight: 0,
+    };
