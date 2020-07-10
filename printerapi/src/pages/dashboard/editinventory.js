3610cd68623d49ef46ac9df97b48726650e9ec49
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-10 00:09:36 -0700

    import dependencies

--- printerapi/src/pages/dashboard/editinventory.js
+++ printerapi/src/pages/dashboard/editinventory.js
@@ -1,3 +1,6 @@
+import React, { PureComponent } from "react";
+import style from "./index.module.scss";
+import firebase from "../../database/firebase";
     this.state = {
       paper: [],
       newpaperid: "",
