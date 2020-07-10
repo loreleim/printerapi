581a424b4b95dd42a1ac790a9bfe87cb532bd658
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-10 00:09:55 -0700

    look into if PureComponent is best

--- printerapi/src/pages/dashboard/editinventory.js
+++ printerapi/src/pages/dashboard/editinventory.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from "react";
 import style from "./index.module.scss";
 import firebase from "../../database/firebase";
+
+export default class EditInventory extends PureComponent {
     this.state = {
       paper: [],
       newpaperid: "",
