33c544f0577a9c9248abbfdb31f937244fe0182f
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-09 22:50:09 -0700

    pulls in edit inventory component

--- printerapi/src/pages/dashboard/index.js
+++ printerapi/src/pages/dashboard/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import style from "./index.module.scss";
+import EditInventory from "./editinventory";
 
 class Dashboard extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class Dashboard extends React.Component {
       <div className={style.mainContainer}>
         <section className={style.dashContainer}>
           <h1>Dashboard</h1>
+          <EditInventory></EditInventory>
         </section>
       </div>
     );
