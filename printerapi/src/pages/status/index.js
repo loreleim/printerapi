a2c61623d8e0a522c58027a0f2d8ae76972abe0f
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-11 01:22:38 -0700

    working basic status checker

--- printerapi/src/pages/status/index.js
+++ printerapi/src/pages/status/index.js
@@ -1,12 +1,60 @@
 import React from "react";
 import style from "./index.module.scss";
+import firebase from "../../database/firebase";
 
 class Status extends React.Component {
+  constructor(props) {
+    super(props);
+    this.test = ["test", "something"];
+    this.state = {
+      suggestions: [],
+      OrderNumber: "",
+      orderStatus: [],
+    };
+  }
+
+  searchStatus = (e) => {
+    firebase
+      .firestore()
+      .collection("orders")
+      .doc(`${this.state.OrderNumber}`)
+      .onSnapshot((doc) => {
+        const replace = doc.data().status;
+        this.setState({ orderStatus: replace });
+      });
+    e.preventDefault();
+  };
+
+  handleChange = (e) => {
+    const name = e.target.name;
+    this.setState({ [name]: e.target.value });
+    console.log(this.state.OrderNumber);
+  };
+
+  renderMatches() {
+    if (this.state.orderStatus.length === 0) {
+      return null;
+    }
+    return <div>{this.state.orderStatus}</div>;
+  }
+
   render() {
     return (
-      <div className={style.mainContainer}>
+      <div className={style.statusContainer}>
         <section className={style.heroContainer}>
-          <h1>Status</h1>
+          <h1>View Order Status</h1>
+          <form onSubmit={this.searchStatus}>
+            <label>
+              <input
+                name={"OrderNumber"}
+                placeholder="search by order number"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </label>
+            <button type="submit">Search</button>
+          </form>
+          {this.renderMatches()}
         </section>
       </div>
     );
