9e112d9ad5caef9e8e8d1ca990828fc34b02ae47
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-10 18:53:42 -0700

    load user data component

--- printerapi/src/pages/dashboard/index.js
+++ printerapi/src/pages/dashboard/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./index.module.scss";
 import firebase from "../../database/firebase";
 import EditInventory from "./editinventory";
+import UserData from "./userdata";
 
 class Dashboard extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Dashboard extends React.Component {
       <div className={style.mainContainer}>
         <section className={style.dashContainer}>
           <h1>Dashboard</h1>
+          <UserData></UserData>
           <button onClick={() => firebase.auth().signOut()}>Sign out</button>
           <EditInventory></EditInventory>
         </section>
