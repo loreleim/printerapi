7888c5fe7f27a85aab44f13342988bd4be5ce6b8
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-10 23:29:57 -0700

    moving components into their own file

--- printerapi/src/pages/dashboard/editinventory.js
+++ printerapi/src/pages/dashboard/editinventory.js
@@ -7,13 +7,6 @@ export default class EditInventory extends PureComponent {
     super(props);
     this.state = {
       paper: [],
-      newpaperid: "",
-      PaperName: "",
-      NewDescription: "",
-      NewReams: 0,
-      NewSheetsPerReam: 0,
-      NewUses: [],
-      NewWeight: 0,
     };
   }
 
@@ -30,25 +23,6 @@ export default class EditInventory extends PureComponent {
       });
   }
 
-  addPaper = () => {
-    firebase
-      .firestore()
-      .collection("paper")
-      .doc(`${this.state.PaperName}`)
-      .set({
-        description: `${this.state.NewDescription}`,
-        reams: `${this.state.NewReams}`,
-        sheetsPerReam: `${this.state.NewSheetsPerReam}`,
-        uses: `${this.state.NewUses}`,
-        weight: `${this.state.NewWeight}`,
-      });
-  };
-
-  handleChange = (e) => {
-    const name = e.target.name;
-    this.setState({ [name]: e.target.value });
-  };
-
   render() {
     console.log("PaperName: " + this.state.PaperName);
     console.log("New Description: " + this.state.NewDescription);
@@ -58,7 +32,7 @@ export default class EditInventory extends PureComponent {
         {this.state.paper.map((paper) => (
           <div key={paper.id} className={style.paperCard}>
             <h2>{paper.id}</h2>
-            <p>{paper.description}</p>
+            <p className={style.paperDescription}>{paper.description}</p>
             <p>Reams Available: {paper.reams}</p>
             <p>Sheets per Ream: {paper.sheetsPerReam}</p>
             <p>Sheets in Stock: {paper.sheetsPerReam * paper.reams}</p>
@@ -66,56 +40,6 @@ export default class EditInventory extends PureComponent {
             <p>Weight: {paper.weight}</p>
           </div>
         ))}
-        <label>
-          New Paper Name
-          <input
-            type="text"
-            name={"PaperName"}
-            onChange={this.handleChange}
-          ></input>
-        </label>
-        <label>
-          Description
-          <input
-            type="text"
-            name={"NewDescription"}
-            onChange={this.handleChange}
-          ></input>
-        </label>
-        <label>
-          How Many Reams In Stock?
-          <input
-            type="text"
-            name={"NewReams"}
-            onChange={this.handleChange}
-          ></input>
-        </label>
-        <label>
-          How Many Sheets in a Ream?
-          <input
-            type="text"
-            name={"NewSheetsPerReam"}
-            onChange={this.handleChange}
-          ></input>
-        </label>
-        <label>
-          What do people use this paper for?
-          <input
-            type="text"
-            name={"NewUses"}
-            onChange={this.handleChange}
-          ></input>
-        </label>
-        <label>
-          What is the GSM of the Paper?
-          <input
-            type="number"
-            name={"NewWeight"}
-            onChange={this.handleChange}
-          ></input>
-        </label>
-        <button onClick={this.addPaper}>Add New Paper</button>
-        <div className={style.navSpacer}></div>
       </div>
     );
   }
