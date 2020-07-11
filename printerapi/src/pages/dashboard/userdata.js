eb19909714c5333119aa1e471c8a54b575bdbe9a
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-10 18:54:26 -0700

    get the user name from current user ID

--- printerapi/src/pages/dashboard/userdata.js
+++ printerapi/src/pages/dashboard/userdata.js
@@ -1,3 +1,7 @@
+import React, { PureComponent } from "react";
+//import style from "./index.module.scss";
+import firebase from "../../database/firebase";
+
 export default class UserData extends PureComponent {
   constructor(props) {
     super(props);
@@ -5,3 +9,16 @@ export default class UserData extends PureComponent {
       user: [],
     };
   }
+
+  componentDidMount() {
+    const userID = firebase.auth().currentUser.uid;
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(userID)
+      .onSnapshot((doc) => {
+        const replace = doc.data().name;
+        this.setState({ user: replace });
+      });
+  }
+
