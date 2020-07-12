1633b63633d41e021a754dbbdb70b8d71f2e5855
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-07-12 15:28:25 -0700

    working conditional render for user nav

--- printerapi/src/components/nav/index.js
+++ printerapi/src/components/nav/index.js
@@ -1,11 +1,74 @@
 import React, { PureComponent } from "react";
 import { Link } from "react-router-dom";
 import style from "./index.module.scss";
+import firebase from "../../database/firebase";
 
 export default class Nav extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ isLoggedIn: true });
+      } else {
+        this.setState({ isLoggedIn: false });
+      }
+    });
+  }
+
   render() {
     return (
       <div>
+        {this.state.isLoggedIn ? (
+          <nav className={style.mainNav}>
+            <ul>
+              <li>{this.state.message}</li>
+              <li>
+                <Link to={"/dashboard"}>Dashboard</Link>
+              </li>
+              <li>
+                <Link to={"/order"}>Order</Link>
+              </li>
+              <li>
+                <Link to={"/status"}>Status</Link>
+              </li>
+              <li>
+                <Link to={"/inventory"}>Inventory</Link>
+              </li>
+              <li>
+                <Link to={"/estimate"}>Estimate</Link>
+              </li>
+            </ul>
+          </nav>
+        ) : (
+          <nav className={style.mainNav}>
+            <ul>
+              <li>{this.state.message}</li>
+              <li>
+                <Link to={"/"}>Home</Link>
+              </li>
+              <li>
+                <Link to={"/order"}>Order</Link>
+              </li>
+              <li>
+                <Link to={"/status"}>Status</Link>
+              </li>
+              <li>
+                <Link to={"/inventory"}>Inventory</Link>
+              </li>
+              <li>
+                <Link to={"/estimate"}>Estimate</Link>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </div>
+      /*<div>
         <nav
           className={style.mainNav}
           style={{
@@ -15,6 +78,7 @@ export default class Nav extends PureComponent {
           }}
         >
           <ul>
+            <li>{this.state.message}</li>
             <li>
               <Link to={"/"}>Home</Link>
             </li>
@@ -32,7 +96,7 @@ export default class Nav extends PureComponent {
             </li>
           </ul>
         </nav>
-      </div>
+      </div>*/
     );
   }
 }
